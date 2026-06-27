f306dbc76d8dc96d66b883b43738f1fb31872f3c
faryal-shakeel <faryalshakeel1055@gmail.com>
2026-06-27 14:46:00 +0500

    Update settings.py and requirements.py

--- budgethub/settings.py
+++ budgethub/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-y&mp_n8+!_5kcrv#f7hz37c!&3x$o7!*#sn$=pzif%$rv-=nwt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['faryalhub.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -115,7 +116,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+import os
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
